62180fa7cdc2d5115a7bd1a80079cbad5adfa254
TurtlSandwich <i398785@fhict.nl>
2019-09-06 12:50:28 +0200

    Made Program

---
+++ Semester2PersoonlijkProjectStreamLabs/Semester2PersoonlijkProjectStreamLabs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Semester2PersoonlijkProjectStreamLabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
